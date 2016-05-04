ba63245180c23b3456864a7310675b086b3bf206
Anthony Olu <b.ioanth1000@gmail.com>
2016-05-04 17:11:06 +0100

    Initial Commit

---
+++ Customer Locator App/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Customer Locator App
+//
+//  Created by MBPinTheAir on 04/05/2016.
+//  Copyright © 2016 moorsideinc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Customer Locator App/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Customer Locator App
+//
+//  Created by MBPinTheAir on 04/05/2016.
+//  Copyright © 2016 moorsideinc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
